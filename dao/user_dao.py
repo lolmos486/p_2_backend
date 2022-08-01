5b4f737e4720d52ef305bffff7c07f2bd7033d00
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-08-01 14:56:17 -0700

    dao continued

--- dao/user_dao.py
+++ dao/user_dao.py
@@ -1,10 +1,34 @@
+import psycopg
+from model.user import User
 
 
+class UserDao:
+    def __init__(self):
+        pass
 # Create
-
+    def create_user(self, user_obj):
+        with psycopg.connect(host="localhost", port="5432", dbname="postgres", user="postgres",
+                             password="password") as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"INSERT INTO project_2.users (username, password, fav_genre, date_joined VALUES"
+                            f"('{user_obj.usn}', crypt('{user_obj.pwd}', gen_salt('bf')), '{user_obj.fav_genre}', "
+                            f"'{user_obj.joined}';")
 
 
 # Read
+    def get_user(self, username):
+        with psycopg.connect(host="localhost", port="5432", dbname="postgres", user="postgres",
+                             password="password") as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM project_2.users WHERE username = '{username}';")
+                for line in cur:
+                    user = User(line[1], line[2], line[3], line[4])
+                    user.set_id(line[0])
+                    if line[5]:
+                        user.set_admin_true()
+                    return user
+
+
 
 # Update
 
