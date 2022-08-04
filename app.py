a45d874a804c73b29710aa98c9beab764f6f45e2
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-08-04 14:36:24 -0700

    update app with secret key

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from controller.book_controller import bc
 
 
 if __name__ == '__main__':
+
     app = Flask(__name__)
+    app.secret_key = "hello"
+
     app.register_blueprint(uc)
     app.register_blueprint(bc)
     app.register_blueprint(rc)
