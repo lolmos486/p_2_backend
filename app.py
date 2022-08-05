4972257882e1d7a913bffe5457689a9d3f7d0fef
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-08-05 08:10:39 -0700

    mortal combat with app. Troops decimated. Reordered, reset, made work.

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from flask_session import Session
+from flask_cors import CORS
 from controller.user_controller import uc
 from controller.review_controller import rc
 from controller.book_controller import bc
 
 
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
     app = Flask(__name__)
-    app.secret_key = "hello"
+    app.secret_key = 'hello'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    # app.config['SECRET_KEY'] = 'hello'
+    app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 
     app.register_blueprint(uc)
     app.register_blueprint(bc)
     app.register_blueprint(rc)
 
+    CORS(app)
+    Session(app)
     app.run(port=8080, debug=True)
